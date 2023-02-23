08dc925d12f94e41f3151eb580f78c9e52a78529
daangemist <git@daangemist.nl>
2023-02-23 21:25:27 +0100

    Wrap the initialization so that it invokes a local start() instead of a global one which can be replaced by a different mm module.

--- web/web.js
+++ web/web.js
@@ -1,12 +1,14 @@
 // Invoke the teletronRegisterExtension global to register the module.
 
-function start() {
-  if (typeof magicMirrorWrapperWeb === 'undefined') {
-    // Wait until the magicMirrorWrap global is available
-    setTimeout(() => start(), 100);
-    return;
+(function () {
+  function start() {
+    if (typeof magicMirrorWrapperWeb === 'undefined') {
+      // Wait until the magicMirrorWrap global is available
+      setTimeout(() => start(), 100);
+      return;
+    }
+    const teletron = window.teletronRegisterExtension('mm-clock');
+    magicMirrorWrapperWeb.start(teletron, 'clock', 'clock');
   }
-  const teletron = window.teletronRegisterExtension('mm-clock');
-  magicMirrorWrapperWeb.start(teletron, 'clock', 'clock');
-}
-start();
+  start();
+})();
